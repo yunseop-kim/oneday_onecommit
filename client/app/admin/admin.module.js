c77a9b4faee71d5c1beedcae23e40b95f5b26992
Yunseop Kim <yunsub2@gmail.com>
2017-02-11 14:43:44 +0900

    싹 다 밀고 새롭게 시작.

--- client/app/admin/admin.module.js
+++
@@ -1,3 +0,0 @@
-'use strict';
-
-angular.module('onedayOnecommitProjectApp.admin', ['onedayOnecommitProjectApp.auth', 'ui.router']);
--- client/components/navbar/navbar.directive.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('onedayOnecommitProjectApp')
-  .directive('navbar', () => ({
-    templateUrl: 'components/navbar/navbar.html',
-    restrict: 'E',
-    controller: 'NavbarController',
-    controllerAs: 'nav'
-  }));
