ae43883df3e5163771a120082c1058ecb521e162
Yunseop Kim <yunsub2@gmail.com>
2017-07-13 21:45:04 +0900

    1. 밀린 TIL 커밋

---
+++ TIL/TIL20170617/sum.test.js
@@ -0,0 +1,148 @@
+import {
+    sum
+} from './sum';
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).toBe(3);
+});
+
+test('object assignment', () => {
+    const data = {
+        one: 1
+    };
+    data['two'] = 2;
+    expect(data).toEqual({
+        one: 1,
+        two: 2
+    });
+});
+
+test('adding positive numbers is not zero', () => {
+    for (let a = 1; a < 10; a++) {
+        for (let b = 1; b < 10; b++) {
+            expect(a + b).not.toBe(0);
+        }
+    }
+});
+
+test('null', () => {
+    const n = null;
+    expect(n).toBeNull();
+    expect(n).toBeDefined();
+    expect(n).not.toBeUndefined();
+    expect(n).not.toBeTruthy();
+    expect(n).toBeFalsy();
+});
+
+test('zero', () => {
+    const z = 0;
+    expect(z).not.toBeNull();
+    expect(z).toBeDefined();
+    expect(z).not.toBeUndefined();
+    expect(z).not.toBeTruthy();
+    expect(z).toBeFalsy();
+});
+
+test('two plus two', () => {
+    const value = 2 + 2;
+    expect(value).toBeGreaterThan(3);
+    expect(value).toBeGreaterThanOrEqual(3.5);
+    expect(value).toBeLessThan(5);
+    expect(value).toBeLessThanOrEqual(4.5);
+
+    // toBe and toEqual are equivalent for numbers
+    expect(value).toBe(4);
+    expect(value).toEqual(4);
+});
+
+test('adding floating point numbers', () => {
+    const value = 0.1 + 0.2;
+    expect(value).not.toBe(0.3); // It isn't! Because rounding error
+    expect(value).toBeCloseTo(0.3); // This works.
+});
+
+test('there is no I in team', () => {
+    expect('team').not.toMatch(/I/);
+});
+
+test('but there is a "stop" in Christoph', () => {
+    expect('Christoph').toMatch(/stop/);
+});
+
+test('the shopping list has beer on it', () => {
+
+    const shoppingList = [
+        'diapers',
+        'kleenex',
+        'trash bags',
+        'paper towels',
+        'beer',
+    ];
+    expect(shoppingList).toContain('beer');
+});
+
+// function compileAndroidCode() {
+//   throw new ConfigError('you are using the wrong JDK');
+// }
+
+// test('compiling android goes as expected', () => {
+//   expect(compileAndroidCode).toThrow();
+//   expect(compileAndroidCode).toThrow(ConfigError);
+
+//   // You can also use the exact error message or a regexp
+//   expect(compileAndroidCode).toThrow('you are using the wrong JDK');
+//   expect(compileAndroidCode).toThrow(/JDK/);
+// });
+
+
+// async code
+
+test('the data is peanut butter', () => {
+  expect.assertions(1);
+  return fetchData().then(data => {
+    expect(data).toBe('peanut butter');
+  });
+});
+
+test('the fetch fails with an error', () => {
+  expect.assertions(1);
+  return fetchData().catch(e =>
+    expect(e).toMatch('error')
+  );
+});
+
+test('the data is peanut butter', () => {
+  expect.assertions(1);
+  return expect(fetchData()).resolves.toBe('peanut butter');
+});
+
+test('the fetch fails with an error', () => {
+  expect.assertions(1);
+  return expect(fetchData()).rejects.toMatch('error');
+});
+
+test('the data is peanut butter', async () => {
+  expect.assertions(1);
+  const data = await fetchData();
+  expect(data).toBe('peanut butter');
+});
+
+test('the fetch fails with an error', async () => {
+  expect.assertions(1);
+  try {
+    await fetchData();
+  } catch (e) {
+    expect(e).toMatch('error');
+  }
+});
+
+test('the data is peanut butter', async () => {
+  expect.assertions(1);
+  await expect(fetchData()).resolves.toBe('peanut butter');
+});
+
+test('the fetch fails with an error', async () => {
+  expect.assertions(1);
+  await expect(fetchData()).rejects.toMatch('error');
+});
+
